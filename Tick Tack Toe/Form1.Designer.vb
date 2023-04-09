2ce0c1b90e68f9e07e2c19f642ec8adce3d0eddb
simranj-git <130274353+simranj-git@users.noreply.github.com>
2023-04-09 17:09:53 +0530

    Add files via upload

---
+++ Tick Tack Toe/Form1.Designer.vb
@@ -0,0 +1,346 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(26, 114)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(110, 105)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(142, 114)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(110, 105)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(258, 114)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(110, 105)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(26, 225)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(110, 105)
+        Me.Button4.TabIndex = 3
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(142, 225)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(110, 105)
+        Me.Button5.TabIndex = 4
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(258, 225)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(110, 105)
+        Me.Button6.TabIndex = 5
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(26, 336)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(110, 105)
+        Me.Button7.TabIndex = 6
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(142, 336)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(110, 105)
+        Me.Button8.TabIndex = 7
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(258, 336)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(110, 105)
+        Me.Button9.TabIndex = 8
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(397, 352)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(131, 53)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "NEW GAME"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(397, 156)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(131, 50)
+        Me.Button11.TabIndex = 10
+        Me.Button11.Text = "RESET"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(397, 250)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(131, 54)
+        Me.Button12.TabIndex = 11
+        Me.Button12.Text = "EXIT"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(11, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(124, 39)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Player X :"
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft YaHei", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(392, 28)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(117, 41)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Player O :"
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Mistral", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Fuchsia
+        Me.Label4.Location = New System.Drawing.Point(136, 27)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(92, 41)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("MV Boli", 24.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Fuchsia
+        Me.Label3.Location = New System.Drawing.Point(8, 481)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(520, 52)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "NOUGHTS and CROSSES"
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Mistral", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Fuchsia
+        Me.Label5.Location = New System.Drawing.Point(515, 28)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(92, 41)
+        Me.Label5.TabIndex = 17
+        Me.Label5.Text = "0"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Font = New System.Drawing.Font("Monotype Corsiva", 28.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Label6.Location = New System.Drawing.Point(234, 16)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(95, 56)
+        Me.Label6.TabIndex = 18
+        Me.Label6.Text = "V/S"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(639, 553)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Button5.Enabled = True
+        Button6.Enabled = True
+        Button7.Enabled = True
+        Button8.Enabled = True
+        Button9.Enabled = True
+
+        Button1.Text = ""
+        Button2.Text = ""
+        Button3.Text = ""
+        Button4.Text = ""
+        Button5.Text = ""
+        Button6.Text = ""
+        Button7.Text = ""
+        Button8.Text = ""
+        Button9.Text = ""
+
+        Label4.Text = "0"
+        Label5.Text = "0"
+
+        Button1.BackColor = Color.White
+        Button2.BackColor = Color.White
+        Button3.BackColor = Color.White
+        Button4.BackColor = Color.White
+        Button5.BackColor = Color.White
+        Button6.BackColor = Color.White
+        Button7.BackColor = Color.White
+        Button8.BackColor = Color.White
+        Button9.BackColor = Color.White
+
+
+
+    End Sub
+
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Color_Change()
+
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Button5.Enabled = True
+        Button6.Enabled = True
+        Button7.Enabled = True
+        Button8.Enabled = True
+        Button9.Enabled = True
+
+        Button1.Text = ""
+        Button2.Text = ""
+        Button3.Text = ""
+        Button4.Text = ""
+        Button5.Text = ""
+        Button6.Text = ""
+        Button7.Text = ""
+        Button8.Text = ""
+        Button9.Text = ""
+
+        Button1.BackColor = Color.White
+        Button2.BackColor = Color.White
+        Button3.BackColor = Color.White
+        Button4.BackColor = Color.White
+        Button5.BackColor = Color.White
+        Button6.BackColor = Color.White
+        Button7.BackColor = Color.White
+        Button8.BackColor = Color.White
+        Button9.BackColor = Color.White
+
+        Button10.Enabled = True
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+    Friend WithEvents Label6 As Label
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
---
+++ Tick Tack Toe/Form1.vb
@@ -0,0 +1,315 @@
+﻿Public Class Form1
+
+    Dim plusone As Integer
+    Dim check As Boolean
+    Private Sub Color_Change()
+        Button1.ForeColor = Color.Black
+        Button2.ForeColor = Color.Black
+        Button3.ForeColor = Color.Black
+        Button4.ForeColor = Color.Black
+        Button5.ForeColor = Color.Black
+        Button6.ForeColor = Color.Black
+        Button7.ForeColor = Color.Black
+        Button8.ForeColor = Color.Black
+        Button9.ForeColor = Color.Black
+    End Sub
+    Private Sub Enabled_False()
+        Button1.Enabled = False
+        Button2.Enabled = False
+        Button3.Enabled = False
+        Button4.Enabled = False
+        Button5.Enabled = False
+        Button6.Enabled = False
+        Button7.Enabled = False
+        Button8.Enabled = False
+        Button9.Enabled = False
+    End Sub
+    Private Sub score()
+
+        'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+        If (Button1.Text = "X" And Button2.Text = "X" And Button3.Text = "X") Then
+            Button1.BackColor = Color.Tomato
+            Button2.BackColor = Color.Tomato
+            Button3.BackColor = Color.Tomato
+            MessageBox.Show("Player X Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label4.Text = Label4.Text + plusone
+            Color_Change()
+            Enabled_False()
+        End If
+
+        If (Button1.Text = "X" And Button4.Text = "X" And Button7.Text = "X") Then
+            Button1.BackColor = Color.Tomato
+            Button4.BackColor = Color.Tomato
+            Button7.BackColor = Color.Tomato
+            MessageBox.Show("Player X Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label4.Text = Label4.Text + plusone
+            Color_Change()
+            Enabled_False()
+        End If
+
+        If (Button1.Text = "X" And Button5.Text = "X" And Button9.Text = "X") Then
+            Button1.BackColor = Color.Tomato
+            Button5.BackColor = Color.Tomato
+            Button9.BackColor = Color.Tomato
+            MessageBox.Show("Player X Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label4.Text = Label4.Text + plusone
+            Color_Change()
+            Enabled_False()
+        End If
+
+        If (Button2.Text = "X" And Button5.Text = "X" And Button8.Text = "X") Then
+            Button2.BackColor = Color.Tomato
+            Button5.BackColor = Color.Tomato
+            Button8.BackColor = Color.Tomato
+            MessageBox.Show("Player X Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label4.Text = Label4.Text + plusone
+            Color_Change()
+            Enabled_False()
+        End If
+
+        If (Button3.Text = "X" And Button6.Text = "X" And Button9.Text = "X") Then
+            Button3.BackColor = Color.Tomato
+            Button6.BackColor = Color.Tomato
+            Button9.BackColor = Color.Tomato
+            MessageBox.Show("Player X Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label4.Text = Label4.Text + plusone
+            Color_Change()
+            Enabled_False()
+        End If
+
+        If (Button3.Text = "X" And Button5.Text = "X" And Button7.Text = "X") Then
+            Button3.BackColor = Color.Tomato
+            Button5.BackColor = Color.Tomato
+            Button7.BackColor = Color.Tomato
+            MessageBox.Show("Player X Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label4.Text = Label4.Text + plusone
+            Color_Change()
+            Enabled_False()
+        End If
+
+        If (Button4.Text = "X" And Button5.Text = "X" And Button6.Text = "X") Then
+            Button4.BackColor = Color.Tomato
+            Button5.BackColor = Color.Tomato
+            Button6.BackColor = Color.Tomato
+            MessageBox.Show("Player X Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label4.Text = Label4.Text + plusone
+            Color_Change()
+            Enabled_False()
+        End If
+
+        If (Button7.Text = "X" And Button8.Text = "X" And Button9.Text = "X") Then
+            Button7.BackColor = Color.Tomato
+            Button8.BackColor = Color.Tomato
+            Button9.BackColor = Color.Tomato
+            MessageBox.Show("Player X Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label4.Text = Label4.Text + plusone
+            Color_Change()
+            Enabled_False()
+        End If
+
+
+        'OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+        plusone = 1
+        If (Button1.Text = "O" And Button2.Text = "O" And Button3.Text = "O") Then
+            Button1.BackColor = Color.Tomato
+            Button2.BackColor = Color.Tomato
+            Button3.BackColor = Color.Tomato
+            MessageBox.Show("Player O Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label5.Text = Label5.Text + plusone
+            Enabled_False()
+        End If
+
+        If (Button1.Text = "O" And Button4.Text = "O" And Button7.Text = "O") Then
+            Button1.BackColor = Color.Tomato
+            Button4.BackColor = Color.Tomato
+            Button7.BackColor = Color.Tomato
+            MessageBox.Show("Player O Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label5.Text = Label5.Text + plusone
+            Enabled_False()
+        End If
+
+        If (Button1.Text = "O" And Button5.Text = "O" And Button9.Text = "O") Then
+            Button1.BackColor = Color.Tomato
+            Button5.BackColor = Color.Tomato
+            Button9.BackColor = Color.Tomato
+            MessageBox.Show("Player O Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label5.Text = Label5.Text + plusone
+            Enabled_False()
+        End If
+
+        If (Button2.Text = "O" And Button5.Text = "O" And Button8.Text = "O") Then
+            Button2.BackColor = Color.Tomato
+            Button5.BackColor = Color.Tomato
+            Button8.BackColor = Color.Tomato
+            MessageBox.Show("Player O Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label5.Text = Label5.Text + plusone
+            Enabled_False()
+        End If
+
+        If (Button3.Text = "O" And Button6.Text = "O" And Button9.Text = "O") Then
+            Button3.BackColor = Color.Tomato
+            Button6.BackColor = Color.Tomato
+            Button9.BackColor = Color.Tomato
+            MessageBox.Show("Player O Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label5.Text = Label5.Text + plusone
+            Enabled_False()
+        End If
+
+        If (Button3.Text = "O" And Button5.Text = "O" And Button7.Text = "O") Then
+            Button3.BackColor = Color.Tomato
+            Button5.BackColor = Color.Tomato
+            Button7.BackColor = Color.Tomato
+            MessageBox.Show("Player O Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label5.Text = Label5.Text + plusone
+            Enabled_False()
+        End If
+
+        If (Button4.Text = "O" And Button5.Text = "O" And Button6.Text = "O") Then
+            Button4.BackColor = Color.Tomato
+            Button5.BackColor = Color.Tomato
+            Button6.BackColor = Color.Tomato
+            MessageBox.Show("Player O Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label5.Text = Label5.Text + plusone
+            Enabled_False()
+        End If
+
+        If (Button7.Text = "O" And Button8.Text = "O" And Button9.Text = "O") Then
+            Button7.BackColor = Color.Tomato
+            Button8.BackColor = Color.Tomato
+            Button9.BackColor = Color.Tomato
+            MessageBox.Show("Player O Wins :)", "Tic Tac Toe", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Label5.Text = Label5.Text + plusone
+            Enabled_False()
+        End If
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (check = False) Then
+            Button1.Text = "X"
+            check = True
+        Else
+            Button1.Text = "O"
+            check = False
+        End If
+        score()
+        Button1.Enabled = False
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (check = False) Then
+            Button2.Text = "X"
+            check = True
+        Else
+            Button2.Text = "O"
+            check = False
+        End If
+        score()
+        Button2.Enabled = False
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If (check = False) Then
+            Button3.Text = "X"
+            check = True
+        Else
+            Button3.Text = "O"
+            check = False
+        End If
+        score()
+        Button3.Enabled = False
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If (check = False) Then
+            Button4.Text = "X"
+            check = True
+        Else
+            Button4.Text = "O"
+            check = False
+        End If
+        score()
+        Button4.Enabled = False
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If (check = False) Then
+            Button5.Text = "X"
+            check = True
+        Else
+            Button5.Text = "O"
+            check = False
+        End If
+        score()
+        Button5.Enabled = False
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If (check = False) Then
+            Button6.Text = "X"
+            check = True
+        Else
+            Button6.Text = "O"
+            check = False
+        End If
+        score()
+        Button6.Enabled = False
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If (check = False) Then
+            Button7.Text = "X"
+            check = True
+        Else
+            Button7.Text = "O"
+            check = False
+        End If
+        score()
+        Button7.Enabled = False
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If (check = False) Then
+            Button8.Text = "X"
+            check = True
+        Else
+            Button8.Text = "O"
+            check = False
+        End If
+        score()
+        Button8.Enabled = False
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If (check = False) Then
+            Button9.Text = "X"
+            check = True
+        Else
+            Button9.Text = "O"
+            check = False
+        End If
+        score()
+        Button9.Enabled = False
+    End Sub
+
+
+
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Dim ex As DialogResult
+
+        ex = MessageBox.Show("Confirm if you want to exit :(", "Tic Tac Toe", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If ex = DialogResult.Yes Then
+            Application.Exit()
+        End If
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+End Class
